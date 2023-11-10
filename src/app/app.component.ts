e35bc2cbc93405021950b8a59da6fd792c0a2cf3
Sebastian Matyszewski <sebastian.matyszewski@sharpsoft.pl>
2023-11-10 21:14:15 +0100

    todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    // inne importy
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list-udemy';
+  config: { title: string | null, footer : string | null, date: Date | null, } = {title: null, footer: null, date: null}
+  tasks: Task[] = [
+    {
+      name: 'Siłownia',
+      deadline: '2023-01-02',
+      done: false,
+    },
+    {
+      name: 'Nauka angulara',
+      deadline: '2023-01-02',
+      done: false,
+    },
+    {
+      name: 'sprzątanie kuwety',
+      deadline: '2023-01-02',
+      done: true,
+    },
+  ];
+
+  constructor() {
+    setTimeout( () => {
+      this.config = {
+        title: 'Lista zadań',
+        footer: '© Lista zadań zbudowana w Angularze.',
+        date: new Date(),
+      };
+    }, 500);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import localePl from '@angular/common/locales/pl'
+import { registerLocaleData } from '@angular/common';
 
 import { routes } from './app.routes';
+
+registerLocaleData(localePl);
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
